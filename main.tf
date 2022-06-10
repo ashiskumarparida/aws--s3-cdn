f720e7948c246d5f62a4ca0420d52b2dfb5463ad
ASHIS KUMAR PARIDA <kumar.ashis.1997@gmail.com>
2022-06-11 00:55:23 +0530

    adding

--- main.tf
+++ main.tf
@@ -16,3 +16,44 @@ resource "aws_s3_bucket" "akbackend" {
 #   bucket = "aktk"
 #   acl    = "private"
 #   }
+
+ resource "aws_cloudfront_distribution" "ak" {
+   origin {
+    domain_name = aws_s3_bucket.default.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.default.id}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.default.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = false
+  default_root_object = "index.html"
+
+  aliases = [var.bucket_name]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.default.id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+
+    lambda_function_association {
+      event_type   = "origin-request"
+      include_body = false
+      lambda_arn   = "arn:aws:lambda:us-east-1:225237029829:function:s3-cdn:2"
+    }
+  }
